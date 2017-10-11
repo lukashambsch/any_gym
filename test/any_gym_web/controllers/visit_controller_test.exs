ba7f968a3bd22addce103c3f3752e786d240f531
Lukas Hambsch <lukas.hambsch@gmail.com>
2017-10-11 17:18:38 -0600

    Add visit controller tests.

--- test/any_gym_web/controllers/visit_controller_test.exs
+++ test/any_gym_web/controllers/visit_controller_test.exs
@@ -1,7 +1,7 @@
 defmodule AnyGymWeb.VisitControllerTest do
   use AnyGymWeb.ConnCase, async: true
 
-  test "index/3 responds with location index page" do
+  test "index/3 responds with visit index page" do
     visit = insert(:visit)
     conn = build_conn()
     |> AnyGymWeb.Auth.login(visit.gym_location.user)
@@ -9,5 +9,19 @@ defmodule AnyGymWeb.VisitControllerTest do
     response = conn
     |> get(visit_path(conn, :index))
     |> response(200)
+
+    assert response =~ Phoenix.View.render_to_string(AnyGymWeb.VisitView, "index.html", visits: [visit])
+  end
+
+  test "show/3 responds with visit show page" do
+    visit = insert(:visit)
+    conn = build_conn()
+    |> AnyGymWeb.Auth.login(visit.gym_location.user)
+
+    response = conn
+    |> get(visit_path(conn, :show, visit.id))
+    |> response(200)
+
+    assert response =~ Phoenix.View.render_to_string(AnyGymWeb.VisitView, "show.html", visit: visit)
   end
 end
